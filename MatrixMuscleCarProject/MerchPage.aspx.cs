6065b7a30d5edb117de0528c5a37308d706b6adb
dariusstutts <dstutts1@my.westga.edu>
2020-04-26 22:43:58 -0400

    modified asp pages so that they reference the right directory

--- MatrixMuscleCarProject/MerchPage.aspx.cs
+++ MatrixMuscleCarProject/MerchPage.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace MatrixMuscleCarProject.images
+namespace MatrixMuscleCarProject
 {
     public partial class MerchPage : System.Web.UI.Page
     {
